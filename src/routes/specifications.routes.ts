655a308d9eeeaf323eaf1b1ac2333db5e81428fd
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-03-28 19:37:06 -0300

    modifications in structure and add entitie User

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+
 import { CreateSpecificationController } from '../modules/cars/useCases/creteSpecification/CreateSpecificationController'
 
 const specificationsRoutes = Router()
 
 const createSpecificationController = new CreateSpecificationController()
 
+specificationsRoutes.use(ensureAuthenticated)
 specificationsRoutes.post('/', createSpecificationController.handle)
 
 export { specificationsRoutes }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import { router } from './routes'
 import swaggerFile from './swagger.json'
@@ -7,6 +8,7 @@ import swaggerFile from './swagger.json'
 import './database'
 
 import './shared/container'
+import { AppError } from './errors/AppError'
 
 const app = express()
 
@@ -15,5 +17,18 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal serve error - ${error.message}`
+  })
+})
 
 app.listen(3333, () => console.log('Server is running!'))
