ef4fffc875d4a074fde6992e6e6c4e0cac6b2c61
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-03-28 19:36:26 -0300

    User authenticate using bscrypt and JWT

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from 'jsonwebtoken'
+import { AppError } from "../errors/AppError";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string
+}
+
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401)
+  }
+
+  // Se existir é necessário desestruturar o token que vem na estrutura Beare
+  const [, token] = authHeader.split(' ')
+
+  try {
+    // Se der erro o verify lança uma exceção por isso o trycatch
+    const { sub: user_id } = verify(token, 'dc50239d02d1d3a092e8c12d0084800d') as IPayload // forçando a retirada do sub do token
+
+    // verificar se o usuário existe no repositório
+    const usersRepository = new UsersRepository()
+    const user = await usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401)
+    }
+
+    next()
+  } catch (error) {
+    throw new AppError('Invalid token', 401)
+  }
+
+}
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,63 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from "../../../../errors/AppError";
+
+
+interface IRequest {
+  email: string
+  password: string
+}
+
+interface IResponse {
+  user: {
+    name: string
+    email: string
+  },
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // Verificar se user existe
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('Email or password incorrect!')
+    }
+
+    // Verificar se a senha está correta
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new AppError('Email or password incorrect!')
+    }
+
+    // Se a senha estiver correta gerar o JWT
+    const token = sign({}, 'dc50239d02d1d3a092e8c12d0084800d', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email
+      },
+      token
+    }
+
+    return tokenReturn
+  }
+}
+
+export { AuthenticateUserUseCase }
