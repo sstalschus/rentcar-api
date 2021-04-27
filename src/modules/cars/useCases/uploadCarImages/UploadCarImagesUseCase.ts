681e72e421e08a19b1fcc4115e03efcf131459c6
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-04-26 22:58:59 -0300

    implements profile user

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe"
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository"
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider"
 
 
 interface IRequest {
@@ -13,7 +14,9 @@ class UploadCarImagesUseCase {
 
   constructor(
     @inject('CarsImagesRepository')
-    private carsImageRepository: ICarsImagesRepository
+    private carsImageRepository: ICarsImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) { }
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
@@ -23,6 +26,8 @@ class UploadCarImagesUseCase {
         car_id,
         image
       )
+
+      await this.storageProvider.save(image, 'cars')
     })
   }
 }
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '@config/upload'
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController'
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController'
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
+import { ProfileUserController } from '@modules/accounts/useCases/profileUserUseCase/ProfileUserController'
 
 const usersRoutes = Router()
 
@@ -12,9 +13,12 @@ const uploadAvatar = multer(uploadConfig)
 
 const createUserController = new CreateUserController()
 const updateUserAvatarController = new UpdateUserAvatarController()
+const profileUserController = new ProfileUserController()
 
 usersRoutes.post('/', createUserController.handle)
 
 usersRoutes.patch('/avatar', ensureAuthenticated, uploadAvatar.single('avatar'), updateUserAvatarController.handle)
+
+usersRoutes.get('/profile', ensureAuthenticated, profileUserController.handle)
 
 export { usersRoutes }
