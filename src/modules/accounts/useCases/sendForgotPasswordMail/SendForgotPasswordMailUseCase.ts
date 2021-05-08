53bad20cf19f26ea7535f5458f4ec592fa8f09ca
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-05-07 23:54:32 -0300

    commit send email service from ses

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from 'uuid'
 import { resolve } from 'path'
+import * as dotenv from "dotenv"
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
@@ -8,7 +9,7 @@ import { AppError } from "@shared/errors/AppError";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 
-
+dotenv.config()
 @injectable()
 class SendForgotPasswordMailUseCase {
 
@@ -19,7 +20,7 @@ class SendForgotPasswordMailUseCase {
     private usersTokensRepository: IUsersTokensRepository,
     @inject("DayjsDateProvider")
     private dateProvider: IDateProvider,
-    @inject('EtherealMailProvider')
+    @inject('MailProvider')
     private mailProvider: IMailProvider
   ) { }
 
