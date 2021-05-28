a2c315480218dc0c422d64a4f3bad8a022ed1192
SamStalschus <samuel5stalschus@gmail.com>
2021-05-27 23:43:58 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,7 @@ import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import { router } from './routes'
 import swaggerFile from '../../../swagger.json'
+import cors from 'cors'
 
 import createConnection from '@shared/infra/typeorm'
 
@@ -23,6 +24,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`))
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`))
 
+app.use(cors())
 app.use(router)
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
